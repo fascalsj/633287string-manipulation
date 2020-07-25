d1f1a85ee4599b8a2c74a67b713bba8ffd4faf66
fascal <fascal@doku.com>
2020-07-25 20:49:22 +0700

    update pom

--- src/main/java/com/github/fascalsj/stringmanipulator/StringManipulatorApplication.java
+++ src/main/java/com/github/fascalsj/stringmanipulator/StringManipulatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StringManipulationApplication {
+public class StringManipulatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StringManipulationApplication.class, args);
+		SpringApplication.run(StringManipulatorApplication.class, args);
 	}
 
 }
---
+++ src/test/java/com/github/fascalsj/stringmanipulator/RandomInteger.java
@@ -0,0 +1,4 @@
+package com.github.fascalsj.stringmanipulator;
+
+public class RandomInteger {
+}
